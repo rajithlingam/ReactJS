f0ec08ac17d792b1a1b4790cc623c4f8408120e6
rajithlingam <rajithlingamedu@gmail.com>
2025-01-11 17:34:34 +0530

    useEffect Hook

--- namasteReactjs/foodMall/src/components/Body.js
+++ namasteReactjs/foodMall/src/components/Body.js
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
-import objList from "../utils/objList";
 
 const Body = () => {
-  const [resCardFilter, setResCardFilter] = useState(objList);
+  const [resCardFilter, setResCardFilter] = useState([]);
   console.log(resCardFilter);
+
+  const fetchObjList = async ()=> {
+    const API_objLink = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.664325&lng=78.1460142&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await API_objLink.json();
+    setResCardFilter(json?.data?.cards[4].card?.card?.gridElements?.infoWithStyle?.restaurants);
+  }
+  useEffect( ()=> {fetchObjList();}, [] );
+
   return (
     <div className="body">
       <div className="RC-Filter">
