76d0a838164d4a403fcc8eb2620b8c96d6bdfd81
rajithlingam <rajithlingamedu@gmail.com>
2025-03-31 21:39:09 +0530

    created Signup and SignIn Page

--- namasteReactjs/netflixGPT/src/components/Login.js
+++ namasteReactjs/netflixGPT/src/components/Login.js
@@ -1,22 +1,39 @@
+import { useState } from "react";
 import { netflix_login_bg_img } from "../utils/url";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+
+  const logStatus = () => {
+    setIsSignIn(!isSignIn);
+  };
+
   return (
     <div className="relative min-h-screen">
       <Header />
 
-      {/* Background Image with Overlay */}
       <div className="absolute inset-0 -z-10">
-        <img src={netflix_login_bg_img} className="w-full h-full object-cover" alt="Netflix Background" />
+        <img
+          src={netflix_login_bg_img}
+          className="w-full h-full object-cover"
+          alt="Netflix Background"
+        />
         <div className="absolute inset-0 bg-black opacity-50"></div>
       </div>
 
       <div className="flex items-center justify-center min-h-screen">
-        <form className="w-4/12 p-10 bg-black opacity-80 text-white rounded-md">
-          <h1 className="text-3xl font-semibold mb-4">Sign In</h1>
-          
+        <form className="w-105 p-10 bg-black opacity-80 text-white rounded-md">
+          <h1 className="text-3xl font-semibold mb-4">
+            {isSignIn ? "Sign In" : "Sign Up"}
+          </h1>
+
           <div className="flex flex-col space-y-4">
+           {!isSignIn &&<input
+              type="name"
+              placeholder="Name"
+              className="w-full p-3 border border-gray-500 bg-gray-800 rounded-md focus:outline-none focus:ring-2 focus:ring-white"
+            />}
             <input
               type="email"
               placeholder="Email"
@@ -28,10 +45,15 @@ const Login = () => {
               className="w-full p-3 border border-gray-500 bg-gray-800 rounded-md focus:outline-none focus:ring-2 focus:ring-white"
             />
           </div>
-          
+
           <button className="w-full mt-6 px-3 py-3 bg-red-600 hover:bg-red-700 active:bg-red-800 rounded-md font-semibold">
-            Sign In
+            {isSignIn ? "Sign In" : "Sign Up"}
           </button>
+          <p onClick={logStatus} className="mt-9 cursor-pointer">
+            {isSignIn
+              ? "New to Netflix? Sign up now."
+              : "Already a member? Sign in now."}
+          </p>
         </form>
       </div>
     </div>
