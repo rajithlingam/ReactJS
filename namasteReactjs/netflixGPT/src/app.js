36adc7124d91a71d018910755b1fd96751a23351
rajithlingam <rajithlingamedu@gmail.com>
2025-05-05 16:23:57 +0530

    added redux

--- namasteReactjs/netflixGPT/src/app.js
+++ namasteReactjs/netflixGPT/src/app.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom/client";
 import Body from "./components/Body";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 const App = () => {
   return (
     <div>
-      <Body/>
+      <Provider store={appStore}>
+        <Body />
+      </Provider>
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App/>);
+root.render(<App />);
